e2dd02f0f1bd3d3d74b10c2eb4e4029eb1eef8da
Johannes <johannes.dahlgren@gmail.com>
2013-12-29 22:45:43 +0100

    removed unused imports

--- src/johannes/dahlgren/braelleranus/BraEllerAnusActivity.java
+++ src/johannes/dahlgren/braelleranus/BraEllerAnusActivity.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -16,7 +15,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import johannes.dahlgren.braelleranus.R;
 import johannes.dahlgren.braelleranus.R.color;
 import johannes.dahlgren.braelleranus.R.string;
